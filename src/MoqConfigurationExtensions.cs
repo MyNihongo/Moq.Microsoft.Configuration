bbf805d6a55983b483fe709d8ce289caa648cf1b
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2021-01-04 21:43:07 +0200

    Update MoqConfigurationExtensions.cs

--- src/MoqConfigurationExtensions.cs
+++ src/MoqConfigurationExtensions.cs
@@ -5,7 +5,7 @@ namespace Moq.Microsoft.Configuration
 {
 	public static class MoqConfigurationExtensions
 	{
-		public static ISetup<IEnumerable<T>> SetupEnumerable<T>(this Mock<IConfiguration> @this, string path) =>
+		public static ISetup<IEnumerable<T>> SetupChildren<T>(this Mock<IConfiguration> @this, string path) =>
 			new EnumerableSetup<T>(@this, path);
 
 		public static ISetup<T> SetupValue<T>(this Mock<IConfiguration> @this, string path) =>
