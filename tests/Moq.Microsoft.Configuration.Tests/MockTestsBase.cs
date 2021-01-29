4898ef729491ec56971eaadd5b50d7192345355e
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-01-29 15:21:26 +0200

    delete unneeded class

--- tests/Moq.Microsoft.Configuration.Tests/MockTestsBase.cs
+++ tests/Moq.Microsoft.Configuration.Tests/MockTestsBase.cs
@@ -6,12 +6,5 @@ namespace Moq.Microsoft.Configuration.Tests
 	{
 		protected Mock<IConfiguration> CreateClass() =>
 			new Mock<IConfiguration>();
-
-		protected class Test
-		{
-			public int Index { get; set; }
-
-			public string Text { get; set; }
-		}
 	}
 }
