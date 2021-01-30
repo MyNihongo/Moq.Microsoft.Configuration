1a0b32700a0212c10095d5c26148e9481586c562
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-01-30 12:42:45 +0200

    tests for objects

--- tests/Moq.Microsoft.Configuration.Tests/Setups/SectionSetupTests/ReturnsShould.cs
+++ tests/Moq.Microsoft.Configuration.Tests/Setups/SectionSetupTests/ReturnsShould.cs
@@ -591,15 +591,15 @@ namespace Moq.Microsoft.Configuration.Tests.Setups.SectionSetupTests
 				.Be(value);
 		}
 
-		[Fact]
-		public void SetupClass()
+		[Theory]
+		[InlineData(null)]
+		[InlineData("value")]
+		public void SetupClassWithString(string input)
 		{
 			const string key = nameof(key);
 			var value = new
 			{
-				StringValue = "string",
-				IntValue = 123,
-				BoolValue = true
+				Value = input
 			};
 
 			var fixture = CreateClass();
@@ -608,23 +608,667 @@ namespace Moq.Microsoft.Configuration.Tests.Setups.SectionSetupTests
 				.SetupSection<dynamic>(key)
 				.Returns(value);
 
-			var section = fixture.Object
-				.GetSection(key);
-
-			section
-				.GetValue<string>(nameof(value.StringValue))
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<string>(nameof(value.Value));
+			
+			result	
 				.Should()
-				.Be(value.StringValue);
+				.Be(value.Value);
+		}
 
-			section
-				.GetValue<int>(nameof(value.IntValue))
-				.Should()
-				.Be(value.IntValue);
+		[Fact]
+		public void SetupClassWithBool()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = true
+			};
 
-			section
-				.GetValue<bool>(nameof(value.BoolValue))
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<bool>(nameof(value.Value));
+
+			result
 				.Should()
-				.Be(value.BoolValue);
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData(true)]
+		public void SetupClassWithNullableBool(bool? input)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = input
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<bool?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Fact]
+		public void SetupClassWithByte()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = (byte)123
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<byte>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData((byte)123)]
+		public void SetupClassWithNullableByte(byte? input)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = input
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<byte?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Fact]
+		public void SetupClassWithsbyte()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = (sbyte)123
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<sbyte>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData((sbyte)123)]
+		public void SetupClassWithNullableSbyte(sbyte? input)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = input
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<sbyte?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Fact]
+		public void SetupClassWithChar()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = '日'
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<char>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData('日')]
+		public void SetupClassWithNullableChar(char? input)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = input
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<char?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Fact]
+		public void SetupClassWithDecimal()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = 123m
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<decimal>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData(123)]
+		public void SetupClassWithNullableDecimal(int? intInput)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = intInput switch
+				{
+					null => (decimal?)null,
+					not null => Convert.ToDecimal(intInput)
+				}
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<decimal?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Fact]
+		public void SetupClassWithDouble()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = 123d
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<double>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData(123d)]
+		public void SetupClassWithNullableDouble(double? input)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = input
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<double?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Fact]
+		public void SetupClassWithFloat()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = 123f
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<float>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData(123f)]
+		public void SetupClassWithNullableFloat(float? input)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = input
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<float?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Fact]
+		public void SetupClassWithInt()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = 123
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<int>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData(123)]
+		public void SetupClassWithNullableInt(int? input)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = input
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<int?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Fact]
+		public void SetupClassWithUint()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = 123u
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<uint>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData(123u)]
+		public void SetupClassWithNullableUint(uint? input)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = input
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<uint?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Fact]
+		public void SetupClassWithLong()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = 123L
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<long>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData(123L)]
+		public void SetupClassWithNullableLong(long? input)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = input
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<long?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Fact]
+		public void SetupClassWithUlong()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = 123UL
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<ulong>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData(123UL)]
+		public void SetupClassWithNullableUlong(ulong? input)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = input
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<ulong?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Fact]
+		public void SetupClassWithShort()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = (short)123
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<short>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData((short)123)]
+		public void SetupClassWithNullableShort(short? input)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = input
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<short?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Fact]
+		public void SetupClassWithUshort()
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = (ushort)123
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<ushort>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData((ushort)123)]
+		public void SetupClassWithNullableUshort(ushort? input)
+		{
+			const string key = nameof(key);
+			var value = new
+			{
+				Value = input
+			};
+
+			var fixture = CreateClass();
+
+			fixture
+				.SetupSection<dynamic>(key)
+				.Returns(value);
+
+			var result = fixture.Object
+				.GetSection(key)
+				.GetValue<ushort?>(nameof(value.Value));
+
+			result
+				.Should()
+				.Be(value.Value);
 		}
 
 		[Fact]
