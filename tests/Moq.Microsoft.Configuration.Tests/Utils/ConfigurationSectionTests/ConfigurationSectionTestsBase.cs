7634b1230c5c37bd4e59ee71ec9bff2434511e26
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-07-28 19:23:02 +0300

    add tests for Section

---
+++ tests/Moq.Microsoft.Configuration.Tests/Utils/ConfigurationSectionTests/ConfigurationSectionTestsBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Moq.Microsoft.Configuration.Tests.Utils.ConfigurationSectionTests
+{
+	public abstract class ConfigurationSectionTestsBase
+	{
+		internal static ConfigurationSection CreateClass() =>
+			new("any");
+	}
+}
