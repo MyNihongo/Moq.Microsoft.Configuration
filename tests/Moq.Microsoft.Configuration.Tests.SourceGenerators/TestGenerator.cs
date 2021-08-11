b345972cd98d931a60a3b3aa3517e844b446db8b
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-08-11 21:09:03 +0300

    create a test source generator

---
+++ tests/Moq.Microsoft.Configuration.Tests.SourceGenerators/TestGenerator.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Moq.Microsoft.Configuration.Tests.SourceGenerators
+{
+	[Generator]
+	public sealed class TestGenerator : ISourceGenerator
+	{
+		public void Initialize(GeneratorInitializationContext context)
+		{
+
+		}
+
+		public void Execute(GeneratorExecutionContext context)
+		{
+			const string src =
+@"using System;
+using Xunit;
+
+namespace Moq.Microsoft.Configuration.Tests
+{
+	public sealed class TestGen
+	{
+		[Fact]
+		public void ThrowEx()
+		{
+			throw new Exception(""aaaaaaa!"");
+		}
+	}
+}";
+
+			context.AddSource("TestGen", src);
+		}
+	}
+}
