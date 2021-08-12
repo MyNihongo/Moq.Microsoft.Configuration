09a811e63235d7ea0baf649dbb252a92d275dcf6
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-08-12 17:40:00 +0300

    delete tests for Class value

--- tests/Moq.Microsoft.Configuration.Tests.SourceGenerators/TestGenerator.cs
+++ tests/Moq.Microsoft.Configuration.Tests.SourceGenerators/TestGenerator.cs
@@ -25,8 +25,8 @@ namespace Moq.Microsoft.Configuration.Tests.SourceGenerators
 		{
 			var generators = new ITestGenerator[]
 			{
-				//new ReturnsValueGenerator(),
-				//new ReturnsEnumerableOfValuesGenerator()
+				new ReturnsValueGenerator(),
+				new ReturnsEnumerableOfValuesGenerator(),
 				new ReturnsClassOfValueGenerator()
 			};
 
