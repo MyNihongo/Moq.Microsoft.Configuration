8514bed435b642c13c42c58c92689f065209f5aa
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-03-12 18:41:04 +0200

    get props for dictionary

--- tests/Moq.Microsoft.Configuration.Tests/ConfigurationSetupTests/ReturnsExpandoObjectShould.cs
+++ tests/Moq.Microsoft.Configuration.Tests/ConfigurationSetupTests/ReturnsExpandoObjectShould.cs
@@ -1,10 +1,34 @@
-﻿namespace Moq.Microsoft.Configuration.Tests.ConfigurationSetupTests;
+﻿// ReSharper disable UnusedAutoPropertyAccessor.Local
+namespace Moq.Microsoft.Configuration.Tests.ConfigurationSetupTests;
 
 public sealed class ReturnsExpandoObjectShould : ConfigurationTestsBase
 {
 	[Fact]
 	public void SetExpandoObjectAsRoot()
 	{
+		var expected1 = new Field1
+		{
+			Prop1 = 123,
+			Obj1 = new Field1.Object1
+			{
+				Prop1 = 321,
+				Prop2 = "string"
+			}
+		};
+
+		var expected2 = new Field2
+		{
+			Prop1 = new[] { "string1", "string2" },
+			Prop2 = new Field2.Property2
+			{
+				Nested = new Field2.Property2.NestedRecord
+				{
+					Prop1 = "value"
+				},
+				Prop2 = 123L
+			}
+		};
+
 		const string field1 = nameof(field1),
 			field2 = nameof(field2);
 
@@ -20,7 +44,7 @@ public sealed class ReturnsExpandoObjectShould : ConfigurationTestsBase
 			})
 			.Set(field2, new
 			{
-				Prop1 = "string value",
+				Prop1 = new[] { "string1", "string2" },
 				Prop2 = new
 				{
 					Nested = new
@@ -38,9 +62,52 @@ public sealed class ReturnsExpandoObjectShould : ConfigurationTestsBase
 			.Returns(expandoObject);
 
 		var result1 = fixture.Object
-			.GetSection(field1);
+			.GetSection(field1)
+			.Get<Field1>();
 
 		var result2 = fixture.Object
-			.GetSection(field2);
+			.GetSection(field2)
+			.Get<Field2>();
+
+		result1
+			.Should()
+			.Be(expected1);
+
+		result2
+			.Should()
+			.BeEquivalentTo(expected2);
+	}
+
+	private sealed record Field1
+	{
+		public int Prop1 { get; init; }
+
+		public Object1 Obj1 { get; init; } = new();
+
+		public sealed record Object1
+		{
+			public int Prop1 { get; init; }
+
+			public string Prop2 { get; init; } = string.Empty;
+		}
+	}
+
+	private sealed record Field2
+	{
+		public string[] Prop1 { get; init; } = Array.Empty<string>();
+
+		public Property2 Prop2 { get; init; } = new();
+
+		public sealed record Property2
+		{
+			public NestedRecord Nested { get; init; } = new();
+
+			public long Prop2 { get; init; }
+
+			public sealed record NestedRecord
+			{
+				public string Prop1 { get; init; } = string.Empty;
+			}
+		}
 	}
 }
